8cf66b0c71c21c7508f6462c3bef52392592d018
cangyue <1053387324@qq.com>
2020-10-29 11:08:58 +0800

    优化代码，增加tcp连接的断线重连一次功能

--- tcpClient.py
+++ tcpClient.py
@@ -14,12 +14,19 @@ class TcpClient:
         self.room_id = room_id
 
         self.is_stop = True
-        self.is_room_video_send = True
+        self.is_room_video_send = False
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_socket.connect((self.tcp_server_ip, self.tcp_server_port))
+        print('connection tcp_server success')
+
+    def reconnection(self):
+        print('start reconnection')
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_socket.connect((self.tcp_server_ip, self.tcp_server_port))
+        print('reconnection tcp_server success')
+        self.start()
 
     def __socket_receive(self, data_len):
-        print('socket_recv...')
         result = self.tcp_socket.recv(data_len)
         receive_len = len(result)
         while receive_len < data_len:
@@ -36,23 +43,19 @@ class TcpClient:
             send_file = str(self.camera_id) + '.jpg'
             cv2.imwrite(send_file, img)
 
-            # 发送数据包头
+            # 发送图像数据包头
             file_size = os.path.getsize(send_file)
             packet_header = struct.pack('<BII', 2, file_size + 4, self.camera_id)
-            try:
-                self.tcp_socket.send(packet_header)
-            except Exception:
-                pass
+
             # 发送图像数据
             with open(send_file, 'rb') as img_file:
-                thread = threading.currentThread()
-                print(f'thread {thread.ident} tcp_send...')
-                try:
-                    send_bytes = img_file.read(file_size)
-                    self.tcp_socket.send(send_bytes)
-                    print(f'send {len(send_bytes)}')
-                except Exception as e:
-                    print(str(e))
+                if self.is_room_video_send:
+                    try:
+                        self.tcp_socket.send(packet_header)
+                        self.tcp_socket.send(img_file.read(file_size))
+                        print(f'send {file_size}')
+                    except OSError as e:
+                        print(f'send_img_with_exception: {str(e)}')
 
     def start(self):
         self.is_stop = False
@@ -60,25 +63,39 @@ class TcpClient:
         packet_role = struct.pack('<BIB', 3, 1, 2)
         self.tcp_socket.send(packet_role)
 
-        # t = Thread(target=self.__receive, args=())
-        # t.daemon = True
-        # t.start()
+        # 开始等待接收控制命令
+        t = threading.Thread(target=self.__receive, args=())
+        t.daemon = True
+        t.start()
 
     def stop(self):
         self.is_stop = True
 
     def __receive(self):
         # 等待接收图像传输命令
+        is_except = False
         while not self.is_stop:
-            packet_video_request_header = self.__socket_receive(5)
-            packet_video_request_type, packet_video_request_len = struct.unpack('<BI', packet_video_request_header)
-            if packet_video_request_type == 1:  # 图像传输控制命令包
-                room_id, video_status = struct.unpack('<IB', self.__socket_receive(5))
-                if self.room_id == room_id:
-                    pass
-                    # if video_status == 1:  # 发送此房间图像
-                    #     self.is_room_video_send = True
-                    # else:  # 关闭此房间图像
-                    #     self.is_room_video_send = False
-
-        self.tcp_socket.close()
+            try:
+                packet_video_request_header = self.__socket_receive(5)
+                packet_video_request_type, packet_video_request_len = struct.unpack('<BI', packet_video_request_header)
+                if packet_video_request_type == 1:  # 图像传输控制命令包
+                    room_id, video_status = struct.unpack('<IB', self.__socket_receive(5))
+                    if self.room_id == room_id:
+                        if video_status == 1:  # 发送此房间图像
+                            self.is_room_video_send = True
+                        else:  # 关闭此房间图像
+                            self.is_room_video_send = False
+                        print(f'is_video_send: {self.is_room_video_send}')
+            except ConnectionError:
+                is_except = True
+                break
+        # 尝试重连
+        if is_except:
+            self.is_room_video_send = False
+            try:
+                self.tcp_socket.close()
+            except ConnectionError:
+                print('close last socket with exception')
+            self.reconnection()
+        else:
+            self.tcp_socket.close()
--- webcam_demo_new.py
+++ webcam_demo_new.py
@@ -34,13 +34,6 @@ args = opt
 args.dataset = 'coco'
 
 
-def loop():
-    n = 0
-    while True:
-        yield n
-        n += 1
-
-
 class NeuralNet(nn.Module):
     def __init__(self, input_size):
         super(NeuralNet, self).__init__()
@@ -69,20 +62,19 @@ def aged_status_reset(aged):
 
 def pose_detect_with_video(aged_id, classidx, parse_pose_demo):
     use_aged = ages[aged_id]
-    # classidx = classidx.item()
     classidx = int(classidx)
 
     # detect if a new day come
     now_date = time.strftime('%Y-%m-%dT00:00:00', time.localtime())
     if int(now_date[8:10]) > int(use_aged.date[8:10]):  # a new day
         aged_status_reset(use_aged)
+        parse_pose_demo.is_first_frame = True
     use_aged.date = now_date
 
     if parse_pose_demo.is_first_frame:  # 第一帧，开始计时
         parse_pose_demo.is_first_frame = False
     else:
         last_pose_time = time.time() - parse_pose_demo.last_time  # 上一个状态至今的时间差，单位为s
-        # TODO：需要添加新的一天从新计时的情况。当是新的一天时候，设置is_first_fram=True即可
         if use_aged.status == PoseStatus.Sit.value:
             use_aged.timesit += last_pose_time
             use_aged.timesit = int(float(use_aged.timesit))
@@ -107,7 +99,7 @@ def pose_detect_with_video(aged_id, classidx, parse_pose_demo):
         use_aged.status = PoseStatus.Other.value
 
     # 判断当前状态是否需求报警
-    if use_aged.status == PoseStatus.Down.value:
+    if use_aged.status == PoseStatus.Down.value:  # TODO：这里的给值是不对的，需要赋予识别服务的对应的需要报警的状态值
         use_aged.isalarm = True
     else:
         use_aged.isalarm = False
@@ -124,13 +116,18 @@ class ParsePoseDemo:
         self.is_first_frame = True
         self.last_time = 0
         self.tcp_client = tcp_client
+        self.writer = None
+        self.is_stop = True
 
     def start(self):
         # start a thread to read frames from the file video stream
+        self.is_stop = False
         t = Thread(target=self.parse, args=())
         t.daemon = True
         t.start()
-        return self
+
+    def stop(self):
+        self.is_stop= True
 
     def parse(self):
         print('start parse')
@@ -145,31 +142,17 @@ class ParsePoseDemo:
 
         aligner = AlignPoints()
 
-        # Data writer
-        # save_path = os.path.join(args.outputpath, 'AlphaPose_webcam' + webcam + '.avi')
-
         writer = DataWriter(self.tcp_client, self.save_video, self.output_path, cv2.VideoWriter_fourcc(*'XVID'),
                             fps, frameSize, pos_reg_model=pos_reg_model, aligner=aligner).start()
 
-        # 统计时间使用
-        runtime_profile = {
-            'dt': [],
-            'pt': [],
-            'pn': []
-        }
-
         batch_size = self.detbatch
-        while True:
+        while not self.is_stop:
             try:
-                start_time = getTime()
                 with torch.no_grad():
                     (inps, orig_img, im_name, boxes, scores, pt1, pt2) = det_processor.read()
                     if boxes is None or boxes.nelement() == 0:
                         writer.save(None, None, None, None, None, orig_img, im_name.split('/')[-1])
                         continue
-
-                    ckpt_time, det_time = getTime(start_time)
-                    runtime_profile['dt'].append(det_time)
                     # Pose Estimation
 
                     datalen = inps.size(0)
@@ -183,17 +166,12 @@ class ParsePoseDemo:
                         hm_j = pose_model(inps_j)
                         hm.append(hm_j)
                     hm = torch.cat(hm)
-                    ckpt_time, pose_time = getTime(ckpt_time)
-
                     hm = hm.cpu().data
                     writer.save(boxes, scores, hm, pt1, pt2, orig_img, im_name.split('/')[-1])
                     while not writer.result_Q.empty():
                         result = writer.result_Q.get()
                         print('classidx:', result['result'][0]['class'])
-                        # for re in range(len(result['result'])):
-                        #    print('classidx:', result['result'][re]['class'])
-                        # 遍历本摄像头所在房间的老人信息
-                        # 目前只考虑房间只有一个人，所以只循环一遍
+
                         for aged in self.camera_info.roomInfo.agesInfos:
                             if not aged.id in ages.keys():
                                 ages[aged.id] = PoseInfo(agesInfoId=aged.id,
@@ -201,14 +179,14 @@ class ParsePoseDemo:
                                                          timeStand=0, timeSit=0, timeLie=0, timeDown=0, timeOther=0)
                             if len(result['result']) > 0:
                                 # 更新被监护对象各种状态的时间值
+                                # TODO: 未检测到人的时候也应该更新数据库，只不过状态为NONE
                                 pose_detect_with_video(aged.id, float(result['result'][0]['class']), self)
 
-                            break
+                            break  # TODO：目前只考虑每个房间只有一个人的情况，所有目前一次就跳出了循环
                         # 创建或更新PoseInfo数据库记录
                         pose_url = Conf.Urls.PoseInfoUrl + '/UpdateOrCreatePoseInfo'
                         http_result = HttpHelper.create_item(pose_url, ages[aged.id])
-
-                    ckpt_time, post_time = getTime(ckpt_time)
+                        print(f'update_poseinfo_http_result: {http_result}')
             except KeyboardInterrupt:
                 break
 
@@ -218,6 +196,7 @@ class ParsePoseDemo:
 
 
 ages = {}  # 老人字典
+pose_parse_instance_list = []
 # 获取或设置本机IP地址信息
 local_ip = '192.168.1.60'
 
@@ -240,7 +219,8 @@ if __name__ == "__main__":
 
     current_server = HttpHelper.get_items(get_current_server_url)
     # print(current_server)
-    print(len(current_server.cameraInfos))
+    print(f'current_server.camera_count: {len(current_server.cameraInfos)}')
+
     for camera in current_server.cameraInfos:  # 遍历本服务器需要处理的摄像头
         tcpClient_instance = tcpClient.TcpClient('154.8.225.243', 8008, camera.id, camera.roomInfoId)
         tcpClient_instance.start()
@@ -249,11 +229,16 @@ if __name__ == "__main__":
         temp_task_instance = ParsePoseDemo(camera, 'examples/resttttt', 1, pose_model, pos_reg_model,
                                            tcpClient_instance, False)
         temp_task_instance.start()
-        break
+        pose_parse_instance_list.append(temp_task_instance)
 
     while True:
         try:
-            pass
             time.sleep(1)
         except KeyboardInterrupt:
             break
+
+    for instance in pose_parse_instance_list:
+        instance.stop()
+
+
+
